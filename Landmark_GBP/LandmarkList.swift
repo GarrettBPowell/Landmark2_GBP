9798db1349f75b190b4963e6b8a51ebb5628b5f4
Garrett Powell <garettbpowell@gmail.com>
2020-11-07 14:34:41 -0600

    made it into a list

---
+++ Landmark_GBP/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmark_GBP
+//
+//  Created by Garrett Powell on 11/7/20.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarkData) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
