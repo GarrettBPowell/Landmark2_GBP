59e5c7a8dbb8fab504c94e97d43523c807f6b9c4
Garrett Powell <garettbpowell@gmail.com>
2020-11-07 13:58:47 -0600

    back to where i was?

--- Landmark_GBP/ContentView.swift
+++ Landmark_GBP/ContentView.swift
@@ -9,17 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading){
-        Text("TurtleRock")
-            .font(.title)
-            HStack{
-                Text("Joshua Tree National Park")
+        VStack {
+            MapView()
+                .frame(height:300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading){
+            Text("TurtleRock")
+                .font(.title)
+                HStack{
+                    Text("Joshua Tree National Park")
+                            .font(.subheadline)
+                    Spacer()
+                    Text("California")
                         .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+                }
+                .padding()
             }
-            .padding()
         }
     }
 }
