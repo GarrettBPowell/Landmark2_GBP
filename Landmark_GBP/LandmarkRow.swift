70675242a321f1be8dd48e06e7bf9f1fc020fad9
Garrett Powell <garettbpowell@gmail.com>
2020-11-07 14:15:30 -0600

    e?

---
+++ Landmark_GBP/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmark_GBP
+//
+//  Created by Garrett Powell on 11/7/20.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[1])
+    }
+}
